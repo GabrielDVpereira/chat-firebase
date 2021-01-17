b686ef9347e8548c99df33b189e9bba727dc872e
Gabriel Davi <gdavi.gd@gmail.com>
2021-01-17 15:56:33 -0300

    init message decoration

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -1,3 +1,4 @@
+import 'package:chatAppFirebase/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +22,7 @@ class Messages extends StatelessWidget {
           return ListView.builder(
             reverse: true,
             itemCount: chatDocs.length,
-            itemBuilder: (ctx, i) => Text(
+            itemBuilder: (ctx, i) => MessageBubble(
               chatDocs[i]['text'],
             ),
           );
