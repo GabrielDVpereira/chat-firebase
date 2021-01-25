1917c9d31abbab4ef510706b3b223baf165c0478
Gabriel Davi <gdavi.gd@gmail.com>
2021-01-25 15:36:31 -0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,48 @@
 import 'package:chatAppFirebase/screens/auth_screen.dart';
 import 'package:chatAppFirebase/screens/chat_screen.dart';
+import 'package:chatAppFirebase/screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Chat',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-          backgroundColor: Colors.pink,
-          accentColor: Colors.deepPurple,
-          accentColorBrightness: Brightness.dark,
-          buttonTheme: ButtonTheme.of(context).copyWith(
-              buttonColor: Colors.pink,
-              textTheme: ButtonTextTheme.primary,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20))),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (ctx, userSnapshot) {
-            if (userSnapshot.hasData) {
-              return ChatScreen();
-            } else {
-              return AuthScreen();
-            }
-          },
-        ));
+    final Future<FirebaseApp> _init = Firebase.initializeApp();
+
+    return FutureBuilder(
+      future: _init,
+      builder: (ctx, appSnapshot) => MaterialApp(
+          title: 'Flutter Chat',
+          theme: ThemeData(
+            primarySwatch: Colors.pink,
+            backgroundColor: Colors.pink,
+            accentColor: Colors.deepPurple,
+            accentColorBrightness: Brightness.dark,
+            buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20))),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: appSnapshot.connectionState == ConnectionState.waiting
+              ? SplashScreen()
+              : StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (ctx, userSnapshot) {
+                    if (userSnapshot.hasData) {
+                      return ChatScreen();
+                    } else {
+                      return AuthScreen();
+                    }
+                  },
+                )),
+    );
   }
 }
